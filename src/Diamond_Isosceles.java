56e367a30db69223be15b2c651f77aadd749014b
lipikad <lipikad@INlipikad.local>
2015-07-06 16:23:48 +0530

    Diamond Exercise

--- src/Diamond_Isosceles.java
+++ src/Diamond_Isosceles.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 /**
  * Created by lipikad on 7/6/15.
  */
 public class Diamond_Isosceles {
+    public static void main(String args[])
+    {
+        int n;
+        Scanner in =new Scanner(System.in);
+        Loop l=new Loop();
+        System.out.print("Enter a number: ");
+        n=in.nextInt();
+
+        for (int i=0;i<n;i++)
+        {
+            for(int j=0;j<n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            l.inner(i);
+            l.inner(i-1);
+            System.out.println("");
+        }
+    }
 }
+
+class Loop
+{
+    public void inner(int num)
+    {
+        for(int j=0;j<=num;j++)
+        {
+            System.out.print("*");
+        }
+    }
+}
+
+
